8ddf4375b9b70fbafe486c9683e4dc2054d24be2
Coding in Flow <info@codinginflow.com>
2024-09-18 13:17:52 +0200

    End of part 27

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,8 +1,11 @@
+import Product from "@/components/Product";
+import { Skeleton } from "@/components/ui/skeleton";
 import { delay } from "@/lib/utils";
 import { getWixServerClient } from "@/lib/wix-client.server";
-import { getProductBySlug } from "@/wix-api/products";
+import { getProductBySlug, getRelatedProducts } from "@/wix-api/products";
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
+import { Suspense } from "react";
 import ProductDetails from "./ProductDetails";
 
 interface PageProps {
@@ -46,6 +49,46 @@ export default async function Page({ params: { slug } }: PageProps) {
   return (
     <main className="mx-auto max-w-7xl space-y-10 px-5 py-10">
       <ProductDetails product={product} />
+      <hr />
+      <Suspense fallback={<RelatedProductsLoadingSkeleton />}>
+        <RelatedProducts productId={product._id} />
+      </Suspense>
     </main>
+  );
+}
+
+interface RelatedProductsProps {
+  productId: string;
+}
+
+async function RelatedProducts({ productId }: RelatedProductsProps) {
+  await delay(2000);
+
+  const relatedProducts = await getRelatedProducts(
+    getWixServerClient(),
+    productId,
+  );
+
+  if (!relatedProducts.length) return null;
+
+  return (
+    <div className="space-y-5">
+      <h2 className="text-2xl font-bold">Related Products</h2>
+      <div className="flex grid-cols-2 flex-col gap-5 sm:grid lg:grid-cols-4">
+        {relatedProducts.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function RelatedProductsLoadingSkeleton() {
+  return (
+    <div className="flex grid-cols-2 flex-col gap-5 pt-12 sm:grid lg:grid-cols-4">
+      {Array.from({ length: 4 }).map((_, i) => (
+        <Skeleton key={i} className="h-[26rem] w-full" />
+      ))}
+    </div>
   );
 }
--- src/wix-api/products.ts
+++ src/wix-api/products.ts
@@ -1,3 +1,4 @@
+import { WIX_STORES_APP_ID } from "@/lib/constants";
 import { WixClient } from "@/lib/wix-client.base";
 import { cache } from "react";
 
@@ -69,8 +70,6 @@ export async function queryProducts(
 
 export const getProductBySlug = cache(
   async (wixClient: WixClient, slug: string) => {
-    console.log("getProductBySlug");
-
     const { items } = await wixClient.products
       .queryProducts()
       .eq("slug", slug)
@@ -86,3 +85,44 @@ export const getProductBySlug = cache(
     return product;
   },
 );
+
+export async function getRelatedProducts(
+  wixClient: WixClient,
+  productId: string,
+) {
+  const result = await wixClient.recommendations.getRecommendation(
+    [
+      {
+        _id: "68ebce04-b96a-4c52-9329-08fc9d8c1253", // "From the same categories"
+        appId: WIX_STORES_APP_ID,
+      },
+      {
+        _id: "d5aac1e1-2e53-4d11-85f7-7172710b4783", // Frequenly bought together
+        appId: WIX_STORES_APP_ID,
+      },
+    ],
+    {
+      items: [
+        {
+          appId: WIX_STORES_APP_ID,
+          catalogItemId: productId,
+        },
+      ],
+      minimumRecommendedItems: 3,
+    },
+  );
+
+  const productIds = result.recommendation?.items
+    .map((item) => item.catalogItemId)
+    .filter((id) => id !== undefined);
+
+  if (!productIds || !productIds.length) return [];
+
+  const productsResult = await wixClient.products
+    .queryProducts()
+    .in("_id", productIds)
+    .limit(4)
+    .find();
+
+  return productsResult.items;
+}
